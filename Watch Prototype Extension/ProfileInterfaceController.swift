128cc3d5b4ec5521a5f06abf69b8af61bc385775
aparente99 <aparente8@fordham.edu>
2016-09-12 09:23:16 -0400

    Update ProfileInterfaceController.swift
    Needed to Change Buttons to Labels and change some functions because of that for design

--- Watch Prototype Extension/ProfileInterfaceController.swift
+++ Watch Prototype Extension/ProfileInterfaceController.swift
@@ -11,14 +11,22 @@ import Foundation
 
 
 class ProfileInterfaceController: WKInterfaceController {
-
-    @IBOutlet var eduButton: WKInterfaceButton!
+    
+   /* @IBOutlet var eduButton: WKInterfaceButton!
     @IBOutlet var jobButton: WKInterfaceButton!
     @IBOutlet var intThreeButton: WKInterfaceButton!
     @IBOutlet var intTwoButton: WKInterfaceButton!
     @IBOutlet var intOneButton: WKInterfaceButton!
     @IBOutlet var emojiButton: WKInterfaceButton!
     @IBOutlet var nameButton: WKInterfaceButton!
+    */
+    @IBOutlet var nameLabel: WKInterfaceLabel!
+    @IBOutlet var jobLabel: WKInterfaceLabel!
+    @IBOutlet var eduLabel: WKInterfaceLabel!
+    @IBOutlet var intOneLabel: WKInterfaceLabel!
+    @IBOutlet var intTwoLabel: WKInterfaceLabel!
+    @IBOutlet var intThreeLabel: WKInterfaceLabel!
+    
     
     private var appGroupDefaults = NSUserDefaults(suiteName: "com.pathos.Prototype")!
     var contactListDict:NSDictionary = [:]
@@ -29,38 +37,34 @@ class ProfileInterfaceController: WKInterfaceController {
         contactListDict = self.appGroupDefaults.dictionaryForKey("contactListInfo")!
         // access profileInfo from contListDict using name string that was selected
         var profileInfo: Dictionary<String, String> = contactListDict[(context as? NSCopying)!]! as! Dictionary<String, String>
-        self.eduButton.setTitle(profileInfo["edu"])
-        self.jobButton.setTitle(profileInfo["occ"])
-        self.intOneButton.setTitle(profileInfo["int1"])
-        self.intTwoButton.setTitle(profileInfo["int2"])
-        self.intThreeButton.setTitle(profileInfo["int3"])
-        //self.emojiButton.setTitle("")
-        //self.emojiButton.setBackgroundImageNamed("sadFace")
-        let happyEmoji = "\u{1F604}"
+        self.eduLabel.setText(profileInfo["edu"])
+        setTitle(profileInfo["edu"])
+        self.jobLabel.setText(profileInfo["occ"])
+        self.intOneLabel.setText(profileInfo["int1"])
+        self.intTwoLabel.setText(profileInfo["int2"])
+        self.intThreeLabel.setText(profileInfo["int3"])
+        let happyEmoji = "\u{1F602}"
         let flatEmoji = "\u{1F610}"
         let upsetEmoji = "\u{1F612}"
-        print(upsetEmoji)
         
         let vibe:String = profileInfo["Vibe"]!
         if (vibe == "Neutral"){
-            // set button background to emoji image
-            
-            self.nameButton.setTitle((context as? String)! + flatEmoji)
+            self.nameLabel.setText((context as? String)! + flatEmoji)
         } else if (vibe == "Unhappy"){
-            self.nameButton.setTitle((context as? String)! + upsetEmoji)
+            self.nameLabel.setText((context as? String)! + upsetEmoji)
         } else if (vibe == "Happy"){
-            self.nameButton.setTitle((context as? String)! + happyEmoji)
+            self.nameLabel.setText((context as? String)! + happyEmoji)
         }
         
- }
+    }
     
     // use this function with the false bool in order to present profile Info UI
-
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-
+    
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
@@ -68,20 +72,20 @@ class ProfileInterfaceController: WKInterfaceController {
     
     @IBAction func changeEdu() {
         self.presentTextInputControllerWithSuggestions(nil, allowedInputMode: WKTextInputMode.Plain, completion: { (results) -> Void in
-
+            
             if results != nil && results!.count > 0 { //selection made
                 let aResult = results?[0] as? String
-                self.eduButton.setTitle(aResult)
+                self.eduLabel.setText(aResult)
             }
         })
     }
-
+    
     @IBAction func changeOcc() {
         self.presentTextInputControllerWithSuggestions(nil, allowedInputMode: WKTextInputMode.Plain, completion: { (results) -> Void in
-    
+            
             if results != nil && results!.count > 0 { //selection made
                 let aResult = results?[0] as? String
-                self.jobButton.setTitle(aResult)
+                self.jobLabel.setText(aResult)
             }
         })
     }
@@ -91,7 +95,7 @@ class ProfileInterfaceController: WKInterfaceController {
             
             if results != nil && results!.count > 0 { //selection made
                 let aResult = results?[0] as? String
-                self.intThreeButton.setTitle(aResult)
+                self.intThreeLabel.setText(aResult)
             }
         })
     }
@@ -101,7 +105,7 @@ class ProfileInterfaceController: WKInterfaceController {
             
             if results != nil && results!.count > 0 { //selection made
                 let aResult = results?[0] as? String
-                self.intTwoButton.setTitle(aResult)
+                self.intTwoLabel.setText(aResult)
             }
         })
     }
@@ -111,27 +115,27 @@ class ProfileInterfaceController: WKInterfaceController {
             
             if results != nil && results!.count > 0 { //selection made
                 let aResult = results?[0] as? String
-                self.intOneButton.setTitle(aResult)
+                self.intOneLabel.setText(aResult)
             }
         })
     }
     
-    @IBAction func changeEmoji() {
+   /* @IBAction func changeEmoji() {
         self.presentTextInputControllerWithSuggestions(nil, allowedInputMode: WKTextInputMode.Plain, completion: { (results) -> Void in
             
             if results != nil && results!.count > 0 { //selection made
                 let aResult = results?[0] as? String
-                self.emojiButton.setTitle(aResult)
+                self.emojiLabel.setText(aResult)
             }
         })
-    }
+    }*/
     
     @IBAction func changeName() {
         self.presentTextInputControllerWithSuggestions(nil, allowedInputMode: WKTextInputMode.Plain, completion: { (results) -> Void in
             
             if results != nil && results!.count > 0 { //selection made
                 let aResult = results?[0] as? String
-                self.nameButton.setTitle(aResult)
+                self.nameLabel.setText(aResult)
             }
         })
     }
