b1754edec06b4b0cf12d28c838e83dfc4a9833bc
aparente99 <aparente8@fordham.edu>
2016-09-16 16:34:11 -0400

    Create talkingPointRowController.swift

---
+++ Watch Prototype Extension/talkingPointRowController.swift
@@ -0,0 +1,17 @@
+//
+//  talkingPointRowController.swift
+//  Prototype
+//
+//  Created by Anthony Parente on 9/15/16.
+//  Copyright © 2016 Anthony Parente. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+
+class talkingPointRowController: NSObject{
+    
+    @IBOutlet var talkPointLabel: WKInterfaceLabel!
+    
+    
+}
