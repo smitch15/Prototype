5236142fb3f55a62719863dcbf4af0fe4ce7ea09
aparente99 <aparente8@fordham.edu>
2016-09-16 16:35:08 -0400

    Create profVisualRowController.swift

---
+++ Watch Prototype Extension/profVisualRowController.swift
@@ -0,0 +1,17 @@
+//
+//  profVisualRowController.swift
+//  Prototype
+//
+//  Created by Anthony Parente on 9/16/16.
+//  Copyright © 2016 Anthony Parente. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+
+class profVisualRowController: NSObject{
+    
+    @IBOutlet var talkPointLabel: WKInterfaceLabel!
+    
+    
+}
